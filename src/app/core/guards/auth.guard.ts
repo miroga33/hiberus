edf560c17a9a98118920ddbfd28dbdbde07ce79e
miroga33 <miguelrodriguez.mrg@gmail.com>
2020-03-07 22:33:22 +0100

    redirect guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../authentication/authentication.service';
 
@@ -7,12 +7,16 @@ import { AuthenticationService } from '../authentication/authentication.service'
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private _auth: AuthenticationService) { }
+  constructor(private _auth: AuthenticationService, private router: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot, ): boolean {
 
-    return this._auth.loggedIn();
+    if (this._auth.loggedIn()) {
+      return true;
+    }
+    this.router.navigateByUrl('/home');
+    return false;
   }
 
 }
